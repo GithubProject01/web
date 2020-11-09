ccafc349548e8365e0484af488caab60dfbf1ecc
GithubProject01 <63563137+GithubProject01@users.noreply.github.com>
2020-11-09 14:57:16 -0500

    settogo

---
+++ src/index.js
@@ -0,0 +1,30 @@
+/********************************************************************************* *
+ *  WEB422 – Assignment 3 *
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy. *
+ *  No part of this assignment has been copied manually or electronically from any other source *
+ *  (including web sites) or distributed to other students. *
+ * Name: __Kirti Markan_________ Student ID: _152802187_____ Date: __25 October, 2020______ *
+ * ********************************************************************************/
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
